62c4d9233d4516c87e8693e07e3d9e2892b285ee
zcemycl <yui.leung.15@ucl.ac.uk>
2021-04-11 17:58:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { ProGraph, Navbar } from './components';
 import Particles from 'react-particles-js';
 import particlesConfig from './config/particlesConfig';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
     return (
-        <Router>
+        // <Router>
         <div>     
             <div className={classes.particleBg}>
                 <Particles height="100vh" width="100vw" config={particlesConfig}/>               
@@ -21,7 +21,7 @@ const App = () => {
             {/* </Switch> */}
             
         </div>
-        </Router>
+        // </Router>
     )
 }
 
