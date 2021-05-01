8dd92a607eeb6b301c07e3d19e04f465965a4d97
zcemycl <yui.leung.15@ucl.ac.uk>
2021-05-02 00:23:31 +0100

    update comments

--- src/components/CommentLike/CommentLike.jsx
+++ src/components/CommentLike/CommentLike.jsx
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import { Grid,Card,CardContent,Typography,Divider } from '@material-ui/core';
 import { Provider,LikeButton,ClapButton,UpdownButton } from '@lyket/react';
 import { withStyles } from '@material-ui/core/styles';
+import CommentBox from './CommentBox';
+import Comments from './Comments';
 require('./ably');
 
 
@@ -22,6 +24,7 @@ const useStyles = theme => ({
     maxWidth:'100%',
     height: '100%', 
     textAlign: 'center',
+    overflow: 'scroll',
   },
   cardcontent: {
     paddingBottom: theme.spacing(0),
@@ -32,7 +35,37 @@ const useStyles = theme => ({
 class CommentLike extends Component {
     constructor(props) {
         super(props);
+        this.handleAddComment = this.handleAddComment.bind(this);
         this.state = {comments: []}
+    }
+
+    componentDidMount() {
+        /* global Ably */
+        const channel = Ably.channels.get('comments');
+       
+        channel.attach();
+          channel.once('attached', () => {
+            channel.history((err, page) => {
+              /* create a new array with comments */
+              const comments = Array.from(page.items, item => item.data);
+    
+              this.setState({ comments });
+    
+              /* subscribe to new comments */
+              channel.subscribe((msg, err) => {
+                const commentObject = msg['data'];
+                this.handleAddComment(commentObject);
+              });
+            });
+          });
+    }
+
+    handleAddComment(comment) {
+    this.setState(prevState => {
+        return {
+        comments: [comment].concat(prevState.comments)
+        };
+    });
     }
     
     render() {
@@ -45,7 +78,7 @@ class CommentLike extends Component {
             direction="row"
             spacing={0}
             className={classes.grid}>
-            <Grid xs={12} sm={6} md={4} lg={3}>
+            <Grid xs={12} sm={10} md={8} lg={6}>
                 <Card className={classes.card}>
                 <Provider apiKey={process.env.REACT_APP_LYKET_API_TOKEN}>
                     <CardContent className={classes.cardcontent}>
@@ -72,7 +105,17 @@ class CommentLike extends Component {
                     </CardContent>
                     <Divider light /> 
                 </Provider>
-                
+                <section className="section">
+                    <div className="container">
+                    <div className="columns">
+                        <div className="column is-half is-offset-one-quarter">
+                        <CommentBox handleAddComment={this.handleAddComment} />
+                        <Comments comments={this.state.comments} />
+                        </div>
+                    </div>
+                    </div>
+                </section>
+
                 </Card>
             </Grid>
         </Grid>
