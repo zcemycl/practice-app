616d5ca7fde06d8779375956edc9bedc1011984b
zcemycl <yui.leung.15@ucl.ac.uk>
2021-05-20 00:53:03 +0100

    update

--- src/components/Leaflet/Leaflet.jsx
+++ src/components/Leaflet/Leaflet.jsx
@@ -10,6 +10,7 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import { csv } from "d3-fetch";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import geojsonLondon from './England_London.json'
+import ReactLeafletKml from 'react-leaflet-kml';
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -18,11 +19,14 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+const kmlUrl = 'https://raw.githubusercontent.com/andreafalzetti/uk-counties-list/master/uk-areas/kml/England/England_East_of_England.kml'
 
 const Leaflet = ({setSelected}) => {
     const classes = useStyles();
     const center = [51.505, -0.09]
     const [data,setData] = useState([]);
+    const [Kml,setKml] = useState(null);
+
     useEffect(()=>{
         setSelected("Cluster Map");
     },[setSelected])
@@ -31,7 +35,17 @@ const Leaflet = ({setSelected}) => {
         csv("./data/postcode-outcodes.csv").then(outcodes => {
           setData(outcodes);
         });
-    }, []);
+    },[]);
+
+    useEffect(() => {
+        fetch(kmlUrl)
+            .then((response)=> response.text())
+            .then((kmlText)=>{
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(kmlText,'text/html');
+                setKml(doc);
+            })
+    },[]);
 
     return (
         <div className={classes.content}>
@@ -43,14 +57,16 @@ const Leaflet = ({setSelected}) => {
             className={classes.grid}>
             <Grid xs={12} sm={10} md={8} lg={6} item={true}>
                 <Card className={classes.card}>
-                <MapContainer center={center} zoom={13}>
+                <MapContainer center={center} zoom={9}>
                     <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
+
                     <GeoJSON key="my-geojson" data={geojsonLondon}
                         style={{fillColor:'blue',
                                 fillOpacity: 0.1}}/>
+                    {Kml && <ReactLeafletKml key="my-kml" kml={Kml} />}
                     <MarkerClusterGroup>
                     {
                     data.map(({id,latitude,longitude,postcode})=>
