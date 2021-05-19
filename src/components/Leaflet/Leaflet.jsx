c112ea5027fbdcf2f6de8f677577373600f4949d
zcemycl <yui.leung.15@ucl.ac.uk>
2021-05-19 17:44:23 +0100

    update cluster map

--- src/components/Leaflet/Leaflet.jsx
+++ src/components/Leaflet/Leaflet.jsx
@@ -1,12 +1,29 @@
 import React, {useEffect} from 'react'
 import { Grid, Card } from '@material-ui/core';
 import useStyles from './styles';
+import L from 'leaflet';
+import { MapContainer,TileLayer,Marker,Popup } from 'react-leaflet';
+import './styles.css'
+import 'leaflet/dist/leaflet.css'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 
 const Leaflet = ({setSelected}) => {
     const classes = useStyles();
+    const position = [51.505, -0.09]
+    const center = [51.505, -0.09]
     useEffect(()=>{
         setSelected("Cluster Map");
     },[setSelected])
+    
     return (
         <div className={classes.content}>
         <div className={classes.toolbar}/>
@@ -17,7 +34,17 @@ const Leaflet = ({setSelected}) => {
             className={classes.grid}>
             <Grid xs={12} sm={10} md={8} lg={6} item={true}>
                 <Card className={classes.card} >
-
+                <MapContainer center={center} zoom={13}>
+                    <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>,
                 </Card>
             </Grid>
             
--- src/components/Leaflet/styles.js
+++ src/components/Leaflet/styles.js
@@ -5,8 +5,7 @@ export default makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing(0),
-    position: 'relative',
-    
+    position: 'relative',   
   },
   root: {
     flexGrow: 1,
@@ -18,6 +17,6 @@ export default makeStyles((theme) => ({
     width:'100%',
     height: '100%', 
     textAlign: 'center',
-    background: "transparent",
+    // background: "transparent",
   },
 }));
