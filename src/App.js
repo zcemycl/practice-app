0ecd7b205aed45253217a4d79fa1d857d62b52bc
zcemycl <yui.leung.15@ucl.ac.uk>
2021-05-29 01:52:12 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, {useState,useEffect} from "react";
-import { ProGraph,Navbar,Random,Auth,Chatapp } from './components';
-import { Products,ThreeFiber,CommentLike,NotFound } from './components';
-import { Map,Annotate,Knowledge,Game,ClusterMap } from './components';
-import { Visitors } from './components';
+import { Navbar,Visitors } from './components';
 import Particles from 'react-particles-js';
 import particlesConfig from './config/particlesConfig';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import LoadBalancer from './LoadBalancer'
 import useStyles from './styles';
-import Profile from './components/Auth/pages/Profile';
-import ProtectedRoute from './components/Auth/ProtectedRoute';
+
 import { commerce } from './components/lib/commerce';
 
 const App = () => {
@@ -40,43 +37,10 @@ const App = () => {
             <Navbar selected={selected} setSelected={setSelected}
                 setIsTourOpen={setIsTourOpen} anchorEl={anchorEl}
                 setAnchorEl={setAnchorEl}/>
-            <Switch>
-                <Route exact path="/" render={(props) => (
-                    <Knowledge {...props} setSelected={setSelected}
-                     isTourOpen={isTourOpen} setIsTourOpen={setIsTourOpen}
-                     setAnchorEl={setAnchorEl}/>)}/>
-                <Route exact path="/prograph" render={(props) => (
-                    <ProGraph {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/auth">
-                    <Auth isAuth={isAuth} setIsAuth={setIsAuth}
-                        setSelected={setSelected}/>
-                </Route>
-                <Route exact path="/imgdisplay" render={(props) => (
-                    <Random {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/chatapp" render={(props) => (
-                    <Chatapp {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/3d" render={(props) => (
-                    <ThreeFiber {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/shop">
-                    <Products products={products} setSelected={setSelected}/>
-                </Route>
-                <Route exact path="/map" render={(props) => (
-                    <Map {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/commentlike" render={(props) => (
-                    <CommentLike {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/annotate" render={(props) => (
-                    <Annotate {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/game" render={(props) => (
-                    <Game {...props} setSelected={setSelected}/>)}/>
-                <Route exact path="/clustermap" render={(props) => (
-                    <ClusterMap {...props} setSelected={setSelected}/>)}/>
-                
-                <ProtectedRoute path="/profile" component={Profile}
-                    isAuth={isAuth}/>
-                <Route render={(props) => (
-                    <NotFound {...props} setSelected={setSelected}/> )}/>
-            </Switch>
             
+                <LoadBalancer {...{products,setSelected,isTourOpen,
+                    setIsTourOpen,setAnchorEl,isAuth,setIsAuth}}/>
+                
         </div>
         </Router>
     )
---
+++ src/LoadBalancer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Route,Switch } from 'react-router-dom';
+import { ProGraph,Random,Auth,Chatapp,NotFound } from './components';
+import { Products,ThreeFiber,CommentLike } from './components';
+import { Map,Annotate,Knowledge,Game,ClusterMap } from './components';
+import Profile from './components/Auth/pages/Profile';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
+
+const LoadBalancer = ({products,setSelected,isTourOpen,
+    setIsTourOpen,setAnchorEl,isAuth,setIsAuth}) => {
+    
+    return (
+        <Switch>
+        <Route exact path="/" render={() => (
+            <Knowledge {...{setSelected,isTourOpen,
+                setIsTourOpen,setAnchorEl}}/>)}/>
+        <Route exact path="/prograph" render={() => (
+            <ProGraph {...{setSelected}}/>)}/>
+        <Route exact path="/auth">
+            <Auth {...{isAuth,setIsAuth,setSelected}}/>
+        </Route>
+        <Route exact path="/imgdisplay" render={() => (
+            <Random {...{setSelected}}/>)}/>
+        <Route exact path="/chatapp" render={() => (
+            <Chatapp {...{setSelected}}/>)}/>
+        <Route exact path="/3d" render={() => (
+            <ThreeFiber {...{setSelected}}/>)}/>
+        <Route exact path="/shop">
+            <Products {...{products,setSelected}}/>
+        </Route>
+        <Route exact path="/map" render={() => (
+            <Map {...{setSelected}}/>)}/>
+        <Route exact path="/commentlike" render={() => (
+            <CommentLike {...{setSelected}}/>)}/>
+        <Route exact path="/annotate" render={() => (
+            <Annotate {...{setSelected}}/>)}/>
+        <Route exact path="/game" render={() => (
+            <Game {...{setSelected}}/>)}/>
+        <Route exact path="/clustermap" render={() => (
+            <ClusterMap {...{setSelected}}/>)}/>
+        <ProtectedRoute exact path="/profile" component={Profile}
+            isAuth={isAuth}/>
+        <Route render={() => (<NotFound {...{setSelected}}/>)}/>
+        </Switch>
+    )
+}
+
+export default LoadBalancer
