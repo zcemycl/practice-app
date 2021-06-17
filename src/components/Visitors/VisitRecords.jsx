3f6a94c435b69e990c8a68dc74e0c6feb803acc0
zcemycl <yui.leung.15@ucl.ac.uk>
2021-06-17 22:26:13 +0100

    update

--- src/components/Visitors/VisitRecords.jsx
+++ src/components/Visitors/VisitRecords.jsx
@@ -1,17 +1,28 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import { Grid, Card } from '@material-ui/core';
 import axios from 'axios';
 import { csv } from "d3-fetch";
 import useStyles from './styles';
+import Plot from 'react-plotly.js';
 
 const VisitRecords = ({setSelected}) => {
     const classes = useStyles();
     const [data,setData] = useState([])
     const [numViews,setNumViews] = useState(0);
+    const [visits,setVisits] = useState({});
+    const [noPlaces,setNoPlaces] = useState({});
+    const cardRef = useRef(null);
+    const [w,setW] = useState(640);
     const sheeturi = 'https://sheet.best/api/sheets/82c23d79-9535-4ef8-9970-f59acfed6f0a'
     useEffect(()=>{
         setSelected("Visitor Record");
     },[setSelected])
+
+    useEffect(()=>{
+        if (cardRef){
+            setW(cardRef.current.clientWidth)
+        }
+    },[])
 
     useEffect(() => {
         csv("data/Visitors-Test.csv").then(d => {
@@ -21,12 +32,26 @@ const VisitRecords = ({setSelected}) => {
             for (let i = 0; i < d.length; i++){
                 axios.get('http://www.geoplugin.net/json.gp?ip='+d[i].IP)
                     .then(res=>{
-                        console.log(d[i].IP,res.data.geoplugin_countryName)
+                        setVisits(prev=>{return {...prev,[d[i].IP]:res.data.geoplugin_countryName}})
+                        console.log(res.data)
                     })
             }
+            console.log(visits)
 
         });
     },[]);
+
+    useEffect(() => {
+        const Unique = Object.keys(visits)
+        const Countries = Object.values(visits)
+        var counts = {};
+        for (var i = 0; i < Countries.length; i++) {
+            counts[Countries[i]] = 1 + (counts[Countries[i]] || 0);
+        }
+        setNoPlaces(counts)
+    },[visits])
+
+    
 
     // useEffect(() => {
     //     axios.get(sheeturi)
@@ -45,8 +70,14 @@ const VisitRecords = ({setSelected}) => {
             spacing={0}
             className={classes.grid}>
             <Grid xs={12} sm={10} md={10} lg={10} item={true}>
-                <Card className={classes.card}>                    
-                
+                <Card className={classes.card} ref={cardRef}>                    
+                <Plot
+                    data={[
+                    {type:'bar',x:Object.keys(noPlaces), 
+                        y:Object.values(noPlaces)},
+                    ]}
+                    layout={ {width: w, height: 480, title: 'Unique Visitors Regions'} }
+                />
                 </Card>
                 
             </Grid>
