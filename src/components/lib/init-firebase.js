143fe5e00a3201db4d3a8878188351087934706e
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-16 04:46:41 +0000

    update

--- src/components/lib/init-firebase.js
+++ src/components/lib/init-firebase.js
@@ -7,15 +7,25 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_FIREBASE_APPID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  apiKey: "AIzaSyDu2YhH827CpUFomIJMTVg8B0JhglSPmfY",
+  authDomain: "practice-app-auth.firebaseapp.com",
+  projectId: "practice-app-auth",
+  storageBucket: "practice-app-auth.appspot.com",
+  messagingSenderId: "827272883178",
+  appId: "1:827272883178:web:eec39b49ee9edbbe666093",
+  measurementId: "G-4L7M463KLZ"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
